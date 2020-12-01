b2d4a0f561f3e04efda1d4b1d00a4cd1edb49d97
paul <boutillier.paul@gmail.com>
2020-12-01 11:07:10 +0100

    Alled

--- codage.py
+++ codage.py
@@ -2,11 +2,11 @@
 def build_Frequency_Table(text):
 
     frequency_table = []
-    str_help = ""   #Créer un 'liste' de caractères rencontrés 
+    str_help = ""   #Créer un chaine de caractere constituée des caractères rencontrés 
 
     for char in text:
 
-        if char not in str_help:    #Si le charactère n'est pas dans la 'liste' alors on crée une liste répertoriant son code ASCII et son occurence que l'on ajoute ensuite à la table de fréquences
+        if char not in str_help:    #Si le charactère n'est pas dans la 'str_help' alors on crée une liste répertoriant son code ASCII et son occurence que l'on ajoute ensuite à la table de fréquences
             str_help += char
             frequency = [ord(char), text.count(char)]
             frequency_table.append(frequency)
@@ -14,6 +14,12 @@ def build_Frequency_Table(text):
     return frequency_table
 
 
-# Fonction permettant de créer l'arbre d'huffman
+# Fonction permettant de trier la table de fréquence par ordre croisssant de fréquences
 def build_Huffman_Tree_List(frequency_table):
-    pass
+    return sorted(frequency_table, key=lambda couple: couple[1])
+
+# Fonction retournant un caractère quelconque
+def getCaractQuelconque():
+    return ";"
+
+
--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import codage as cod
 cheminTEXT = "text.json"
 cheminTEXTOUT = "out-text.json"
 
+# Lecture du text à encoder dans le fichier json text.json
 x = lj.read(cheminTEXT, "text")
-print(x)
+print("Texte à encoder :", x)
 
-table = cod.build_Frequency_Table(x)
-print(table)
+table = cod.build_Frequency_Table(x)    # Création de la table de fréquence
+lj.write(cheminTEXTOUT, "table", table) # Ecriture de la table de fréquence dans le fichier json out-text.json
 
-#lj.write(cheminTEXTOUT, "textEncode", x)
+print(cod.build_Huffman_Tree_List(table))
